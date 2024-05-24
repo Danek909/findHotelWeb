936d372176c7af14892d634eceb9c52d36587437
Danek909 <kogdana@gmail.com>
2024-05-24 16:15:49 +0300

    Po fixed bugs with logonization

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import styles from '../Header/heder.module.css'
-import dolalrs from '../image/dolars.png'
-import language from '../image/language.jpg'
-import userPhoto from '../image/userPhoto.png'
-import PhotoUser from '../image/PhotoUser.avif'
+import styles from '../Header/heder.module.css';
+import dolalrs from '../image/dolars.png';
+import language from '../image/language.jpg';
+import userPhoto from '../image/userPhoto.png';
+import avatarka from '../image/Avatarka.jpg';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { GrLanguage } from "react-icons/gr";
 import { PiCurrencyDollarBold } from "react-icons/pi";
@@ -12,13 +12,13 @@ import { FaFacebook, FaYoutube, FaInstagramSquare, FaTelegram } from "react-icon
 import { ConfigProvider, Modal } from 'antd';
 import RegistrationForm from "../Login/RegistrationForm";
 import { cookies, useCookies } from 'react-cookie';
-
+import { Select } from 'antd';
 
 const Header = (props) => {
     const [nav, setNav] = useState(false);
     const [isModalOpen, setModalOpen] = useState(false);
-    // const [cookies, setCookie, removeCookie] = useCookies(['userData']);
-    // const [userData, setUserData] = useState(cookies.userData);
+    const [cookies, setCookie, removeCookie] = useCookies(['userData']);
+    const [userData, setUserData] = useState(cookies.userData);
 
     const openModal = () => {
         setModalOpen(true);
@@ -27,12 +27,12 @@ const Header = (props) => {
         setModalOpen(false);
     };
 
-    // console.log(userData, "userData Heder start");
-    // const exitLogin = () => {
-    //     setUserData(prewDataUser => ({ ...prewDataUser, isLogind: false }))
-    //     setCookie("userData", userData);
-    //     console.log(userData, "userData Heder");
-    // }
+    const exitLogin = () => {
+        const cookieUserData = cookies.userData;
+        cookieUserData.isLogined = 'noAutorized';
+        setCookie("userData", cookieUserData);
+        console.log(cookieUserData, "userData Heder");
+    }
 
     return (
         <header className={styles.sectionHeder}>
@@ -59,20 +59,46 @@ const Header = (props) => {
                     </ul>
                 </div>
 
+
+
+
                 <div className={styles.imgHeder}>
                     <img src={dolalrs} className={styles.dolalrsHeder} alt="Delete" />
-                    <img src={language} className={styles.languageHeder} alt="Delete" />
-                    <img src={userPhoto} onClick={openModal} alt="Delete" />
-                    <div>
-                {/* Це я пробував робити функціонал по розлогінюванні користувача  */}
-                        {/* <button onClick={exitLogin}>exit</button> */}
-                        {/* <p>{userData.username}</p> */}
-                        {/* {cookies.userData.isLogind = 'true' ? (
-                        <img className={styles.PhotoUser} onClick={openModal} src={PhotoUser} alt="delete" />
-                    )
-                        : cookies.userData.isLogind = 'false'(
-                            <img src={userPhoto} onClick={openModal} alt="Delete" />
-                        )} */}
+                        <Select
+                            suffixIcon=""
+                            defaultValue="English"
+                            style={{
+                                width: 60,
+                                height: 20,
+                                marginTop:45,
+                                marginLeft: 5,
+                                marginRight: 10
+                            }}
+                            options={[
+                                { label: <span>En</span>, value: 'English' },
+                                { label: <span>Uk</span>, value: 'Ukrainian' },
+                                { label: <span>PL</span>, value: 'Poland' },
+                            ]} />
+                    <div className={styles.autorizedDiv}>
+                        {cookies.userData.isLogined === 'autorized' ? (
+                            <button type="button" onClick={openModal}>
+                                <img className={styles.PhotoUser} src={avatarka} alt="delete" />
+                            </button>
+                        ) : (
+                            <button type="button" onClick={openModal}>
+                                <img className={styles.userPhoto} src={userPhoto} alt="Delete" />
+                            </button>
+                        )}
+
+                        <Select
+                            defaultValue={userData.username}
+                            style={{
+                                width: 90,
+                                height: 20,
+                            }}
+                            options={[
+                                { label: <button className={styles.buttonExit} type="button" onClick={exitLogin}>Log Out</button>, value: 'English' },
+                            ]} />
                     </div>
 
                     <ConfigProvider theme={{
--- src/Login/LoginForm.jsx
+++ src/Login/LoginForm.jsx
@@ -4,22 +4,24 @@ import styles from "../Login/Login.module.css"
 import { InputOutlined } from "./components/InputOutlined/inputOutLinet";
 import { BsExclamationCircle } from "react-icons/bs";
 import { useCookies } from 'react-cookie';
-import { Navigate } from "react-router-dom";
 
 const LoginForm = (props) => {
     const [cookies, setCookie] = useCookies(['userData']);
     const [userData, setUserData] = useState(cookies.userData);
     const form = useForm({ mode: "onBlur" });
-    const { handleSubmit, formState, setError } = form;
+    const { handleSubmit, formState, setError , clearErrors} = form;
 
+    const resetErors = () =>{
+        clearErrors("errorsMessageLoginForm");
+    }
     const handleLogin = (values) => {
         if (userData.username === values.username && userData.password === values.password) {
-
-            setCookie("userData", userData.isLogined = "test");
-            alert("good");
-            // return <Navigate to={'/homePage'}/>
+            const cookieUserData = cookies.userData;
+            cookieUserData.isLogined = 'autorized';
+            console.log(cookieUserData, 'cookieUserData');
+            setCookie("userData", cookieUserData);
+            window.location.reload();
         } else {
-            alert("Problem");
             setError("errorsMessageLoginForm", { type: "manual", passwordMesage: "Check the password field and username" });
         }
         console.log(userData, "UserData LoginForm");
@@ -31,12 +33,11 @@ const LoginForm = (props) => {
         <div>
             <h1 className={styles.LoginName}>Login</h1>
             <FormProvider {...form}>
-                <form onSubmit={handleSubmit(handleLogin)}>
+                <form onSubmit={handleSubmit(handleLogin)} onChange={resetErors}>
                     <div className={styles.divContLoginForm_1}>
                         <InputOutlined name="username" type="text" placeholder="UserName"
                             rules={{
                                 required: "This field must be filled in",
-                                maxLength: { value: 15, message: "No more than 15 characters" },
                                 minLength: {
                                     value: 2, message: "At least 2 characters!"
                                 }
@@ -47,7 +48,6 @@ const LoginForm = (props) => {
                         <InputOutlined name="password" type="password" placeholder="Password"
                             rules={{
                                 required: "This field must be filled in",
-                                maxLength: { value: 15, message: "No more than 15 characters" },
                                 minLength: {
                                     value: 2, message: "At least 2 characters!"
                                 }
@@ -55,23 +55,7 @@ const LoginForm = (props) => {
                         <p className={styles.divErrors}>{errors?.password?.message} {errors?.password && <BsExclamationCircle />}</p>
                     </div>
                     <div className={styles.erorsBlock}>
-
                         <p>{errors?.errorsMessageLoginForm?.passwordMesage} {errors?.errorsMessageLoginForm && <BsExclamationCircle />} </p>
-                        {/* {values.username === cookies.username || values.password === cookies.password ? (
-                            <h1>You are authorized!</h1>
-                        ) : (
-                            <p>Check the password field and username<HiMiniExclamationCircle /></p>
-                        )} */}
-
-                        {/* {!currentUser ? (
-                            <p>Check the password field and username<HiMiniExclamationCircle /></p>
-                        ) : currentUser.username === undefined && currentUser.password === undefined ? (
-                            <p></p>
-                        ) : (
-                            <h1>You are authorized!</h1>
-                        )} */}
-
-
                     </div>
                     <div className={styles.divLoginFormContBtn}>
                         <button type="submit">Login</button>
